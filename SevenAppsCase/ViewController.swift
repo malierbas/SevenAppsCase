b45d5ef638a0cf45699934d7f861292e53af3f9b
Ali <63293527+malierbas@users.noreply.github.com>
2025-01-30 14:56:27 +0300

    Initial Commit

---
+++ SevenAppsCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCase
+//
+//  Created by Mehmet Ali Erbaş on 1/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
